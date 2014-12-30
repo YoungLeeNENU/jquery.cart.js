4161eb605c7ee887b8f95f40a419f38fd88c4d5a
maverick <maverick@Maverick.(none)>
2014-12-30 22:28:07 +0800

    定制化参数

--- cart.js
+++ cart.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
 	$('#J_cart').slidecart({
-		// location: 'right'
-		// buttonNum: 2
+		position: 'right',
+		top: 87,
+		bottom: 0,
+		height: $(document).height() - 87,
+		handlerWidth: 35,
+		panelWidth: 280,
+		handlerBackground: '#000000',
+		panelBackground: '#383838',
+		mask: true,
+		maskOpacity: 0.4
 	}).addClass('slidecart-test');
 });
--- jquery.cart.js
+++ jquery.cart.js
@@ -5,63 +5,44 @@
  * @license GNU GENERAL PUBLIC LICENSE Version 3
  */
 (function($) {
-	$.fn.slideLeftShow = function (speed, fn) {
+	$.fn.slideShow = function (dir, speed, fn) {
 		return this.each(function() {
-			$(this).show('slide', { direction: 'left' }, speed, fn);
+			$(this).show('slide', { direction: dir }, speed, fn);
 		});
 	};
-	$.fn.slideRightShow = function (speed, fn) {
+	$.fn.slideHide = function (dir, speed, fn) {
 		return this.each(function() {
-			$(this).show('slide', { direction: 'right' }, speed, fn);
-		});
-	};
-	$.fn.slideLeftHide = function (speed, fn) {
-		return this.each(function() {
-			$(this).hide('slide', { direction: 'left' }, speed, fn);
-		});		
-	};
-	$.fn.slideRightHide = function (speed, fn) {
-		return this.each(function() {
-			$(this).hide('slide', { direction: 'right' }, speed, fn);
+			$(this).hide('slide', { direction: dir }, speed, fn);
 		});
 	};
 	$.widget('uicart.slidecart', {
 		options: {
-			location: 'right',    // left, right, top, bottom
+			position: 'right',
+			top: 0,
+			bottom: 0,
+			height: $(document.body).height(),
+			handlerWidth: 35,
+			panelWidth: 250,
+			containerPos: null,
+			handlerPos: null,
+			panelPos: null,
 			handlerBackground: '#000000',
 			panelBackground: '#383838',
-			setLocation: function (pos) {
-				var rtval = new Object();
-				switch (pos) {
-				case 'left':
-					rtval = { top: 0, left: 0, width: '35px', height: '100%' };
-					break;
-				case 'right':
-					rtval = { top: 0, right: 0, width: '35px', height: '100%' };
-					break;
-				case 'top':
-					rtval = { top: 0, right: 0, width: '100%', height: '35px' };
-					break;
-				case 'bottom':
-					rtval = { bottom: 0, right: 0, width: '100%', height: '35px' };
-					break;
-				default:
-					return { top: 0, right: 0, width: '35px', height: '100%' };
-				}
-				return rtval;
-			}
+			mask: true,
+			maskOpacity: 0.5
 		},
 		_create: function () {
+			this._setConPos(this.options.position);
 			this.element.css($.extend({
 				position: 'absolute',
 				zIndex: 7
-			}, { top: 0, right: 0, width: 335, height: '100%' }));
+			}, this.options.containerPos));
 			this._cartHandler = $('<div></div>')
 				.css($.extend({
 					position: 'absolute',
 					zIndex: 9,
 					background: this.options.handlerBackground
-				}, { margin: '0 0 0 300', padding: '0 0 0 0', width: 35, height: '100%' }))
+				}, this.options.handlerPos))
 				.addClass('cart-handler')
 				.appendTo(this.element);
 			this._innerPanel = $('<div></div>')
@@ -69,39 +50,107 @@
 					position: 'absolute',
 					zIndex: 8,
 					background: this.options.panelBackground
-				}, { margin: '0 0 0 0', padding: '0 0 0 35', width: 300, height: '100%' }))
+				}, this.options.panelPos))
 				.addClass('inner-panel')
 				.appendTo(this.element)
 				.hide();
 			this._addHook(this._cartHandler, 'click', this._innerPanel);
 		},
+		_setConPos: function (pos) {
+			switch (pos) {
+			case 'right':
+				this.options.containerPos = {
+					top: this.options.top,
+					bottom: this.options.bottom,
+					right: 0,
+					width: this.options.handlerWidth + this.options.panelWidth,
+					height: this.options.height
+				};
+				this.options.handlerPos = {
+					marginLeft: this.options.panelWidth,
+					padding: 0,
+					width: this.options.handlerWidth,
+					height: this.options.height
+				};
+				this.options.panelPos = {
+					margin: 0,
+					paddingLeft: this.options.handlerWidth,
+					width: this.options.panelWidth,
+					height: this.options.height
+				};
+				break;
+			case 'left':
+				this.options.containerPos = {
+					top: this.options.top,
+					bottom: this.options.bottom,
+					left: 0,
+					width: this.options.handlerWidth + this.options.panelWidth,
+					height: this.options.height
+				};
+				this.options.handlerPos = {
+					marginRight: this.options.panelWidth,
+					padding: 0,
+					width: this.options.handlerWidth,
+					height: this.options.height
+				};
+				this.options.panelPos = {
+					margin: 0,
+					paddingLeft: this.options.handlerWidth,
+					width: this.options.panelWidth,
+					height: this.options.height
+				};
+				break;
+			default:    // right
+				this.options.containerPos = {
+					top: this.bottom.top,
+					bottom: this.options.bottom,
+					right: 0,
+					width: this.options.handlerWidth + this.options.panelWidth,
+					height: this.options.height
+				};
+				this.options.handlerPos = {
+					marginLeft: this.options.panelWidth,
+					padding: 0,
+					width: this.options.handlerWidth,
+					height: this.options.height
+				};
+				this.options.panelPos = {
+					margin: 0,
+					paddingLeft: this.options.handlerWidth,
+					width: this.options.panelWidth,
+					height: this.options.height
+				};
+				break;
+			}
+		},
 		_addHook: function (anchor, event, folddiv) {
-			var mask = document.createElement('div');
+			var mask = document.createElement('div'),
+				that = this;
 			$(mask).css({
 				position : "absolute",
 				backgroundColor: 'black',
-				opacity : 0.5,
-				top : 0,
-				left : 0,
-				width : $(document.body).width(),
+				opacity : this.options.maskOpacity,
+				top : this.options.top,
+				bottom: this.options.bottom,
+				width : $(document).width(),
+				left: 0,
+				right: 0,
 				zIndex : 5
 			}).hide().appendTo(document.body);
 			anchor.on(event, function () {
 				$(this).toggleClass('unfold');
 				if ($(this).hasClass('unfold')) {
-					// $('.cart-handler').toggle('slide', { direction: 'left' }, 'slow');
-					$(mask).css({
-						height : $(document).height()
-					}).fadeIn('slow');					
-					folddiv.slideRightShow('normal', function () {
-						//TODO: 
-					});
+					if (that.options.mask) {
+						$(mask).css({
+							height : $(document).height() - (that.options.top + that.options.bottom)
+						}).fadeIn('normal');
+					}
+					folddiv.slideShow(that.options.position, 'normal');
 				} else {
-					// $('.cart-handler').toggle('slide', { direction: 'left' }, 'normal');
-					$(mask).fadeOut("slow");						
-					folddiv.slideRightHide('fast', function () {
-						//TODO: 
-					});
+					if (that.options.mask) {
+						$(mask).fadeOut("normal");
+					}
+					folddiv.slideHide(that.options.position, 'fast');
 				}
 			});
 		},
