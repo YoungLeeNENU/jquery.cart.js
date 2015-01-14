c61010c1ad7feb760fd1c72c88b01bd430ca9478
maverick <maverick@Maverick.(none)>
2015-01-14 15:08:47 +0800

    jquery.cart.js增加功能

--- cart.js
+++ cart.js
@@ -6,89 +6,32 @@ $(document).ready(function () {
 		height: document.body.clientHeight - 87,
 		mask: true,
 		tags: 2,
-		setCommonTag: function () {    // Control the tags while use the plugin
-			var hgtFrom = 90,
-				hgtTo = 120;
-			
+		// position: 'left',
+		setSingleTag: [ function () {
 			$(this).css({
-				width: 35,
-				height: hgtFrom,
-				cursor: 'pointer'
-			})
-				// .hover(function () {
-				//     if (!$(this).parent().hasClass('unfold')) {
-				//         $(this).animate({ height: hgtTo }, 'slow');
-				//     }
-				// }, function () {
-				//     if (!$(this).parent().hasClass('unfold')) {
-				//         $(this).animate({ height: hgtFrom }, 'slow');
-				//     }
-				// })
-				.click(function () {
-				    if ($(this).hasClass('off')) {
-				        $(this).animate({ height: hgtTo }, 'normal');
-				        $(this).removeClass('off');
-				        $(this).addClass('on');
-				    } else if ($(this).hasClass('on')) {
-				        $(this).animate({ height: hgtFrom }, 'normal');
-				        $(this).removeClass('on');
-				        $(this).addClass('off');
-				    }
-				});
-			
-			return $(this);
-		},
-		setSingleTag: [    // Set performance for single tag
-			function () {
-				$(this).css({ backgroundColor: '#FFA500' })
-					.data({
-						game: 'seer'
-					});
+				backgroundColor: '#FF8C00'
+			}).data({ game: 'seer' });
 		}, function () {
-			$(this).css({ backgroundColor: '#436EEE' })
-				.data({
-					game: 'more'
-				});			
+			$(this).css({
+				backgroundColor: '#4682B4'
+			}).data({ game: 'more' });
 		} ],
 		setItemsArea: function () {
-			$(this).css({
-				width: 297,
-				height: document.body.clientHeight - 50 - 87,
-				// height: 300,
-				overflowY: 'auto',
-				// border: '1px solid black',
-				zIndex: 73,
-				marginLeft: 5,
-				marginTop: 50,
-				backgroundColor: '#5E5E5E'
-			});
-			for(var i = 0; i < 5; i++) {
+			for(var i = 0; i < 10; i++) {
 				var items = $(document.createElement("div")).addClass('item-sample').appendTo($(this));
 				items.item({
-					timeDft: "2014-12-13~2015-01-12"
+					timeDft: "2014-12-13~2015-01-12",
+					gameNameTxt: function () {
+						$(this).text('赛尔号').attr({
+							'game_id': 16,
+							'r_id': 12318
+						});
+					},
+					ItemNameTxt: function () {
+						$(this).text('添加的事件');
+					}					
 				});
 			}
 		}
 	}).addClass('slidecart-test');
-
-	// $('#J_cartItems').find('.items').cartitems({
-	// 	// liNum: 10
-	// 	liNum: 3,
-	// 	data: [{
-	// 		'game_name': 'game1',
-	// 		'items': [{
-	// 			item_name: 'sample1',
-	// 			r_id: 314
-	// 		}, {
-	// 			item_name: 'sample2',
-	// 			r_id: 315
-	// 		}]
-	// 	}, {
-	// 		'game_name': 'game2',
-	// 		'items': [{
-	// 			item_name: 'sample5',
-	// 			r_id: 625
-	// 		}]			
-	// 	}]
-	// });
 });
--- jquery.cart.js
+++ jquery.cart.js
@@ -16,13 +16,6 @@
 		prop[dir] = -offset;
 		return this.animate(prop, speed, easing, fn);
 	};    // DONE
-	$.fn.reSize = function (element) {
-		var that = this,
-			iWinHeight = document.body.clientHeight;
-		$(window).resize(function () {
-			return element.css({ height: document.body.clientHeight - (iWinHeight - that.options.height) });
-		});
-	};    // PENDING
 	$.widget('uicart.slidecart', {
 		options: {
 			position: 'right',
@@ -31,6 +24,8 @@
 			height: document.body.clientHeight,
 			handlerWidth: 35,
 			panelWidth: 250,
+			notifyHgt: 40,
+			BtnHgt: 100,
 			containerPos: null,
 			handlerPos: null,
 			panelPos: null,
@@ -40,37 +35,59 @@
 			maskZIndex: 70,
 			maskOpacity: 0.5,
 			tags: 1,
+			extraInfo: true,
+			unfold: false,
 			setCommonTag: function () {
-				var hgtFrom = 90,
-					hgtTo = 120;
-				
 				$(this).css({
-					width: 35,
-					height: hgtFrom,
-					borderBottom: '1px solid grey',
-					backgroundColor: '#27408B',
-					cursor: 'pointer'
+					float: 'left', width: 35, height: 90, backgroundColor: '#27408B', cursor: 'pointer'
 				});
-				
+				var numInfo = $(document.createElement("div"))
+						.css({ float: 'left', height: 35, width: 35, marginTop: 55 })
+						.addClass("tag-info")
+						.appendTo($(this));
+				var numCon = $(document.createElement("div"))
+						.css({ height: 18, width: 18, marginTop: 7, marginLeft: 7, borderRadius: 20 })
+						.addClass('num-con')
+						.appendTo(numInfo);
+				var numSpan = $(document.createElement("span"))
+						.css({ fontWeight: 'bold', color: '#F5F5F5', marginLeft: 5 })
+						.addClass('num-span')
+						.appendTo(numCon);
 				return $(this);
 			},
-			setSingleTag: [ function () {
-				$(this).css({
-					backgroundColor: '#FF8C00'
-				});
-			} ],
+			commonTagEffect: function () {
+				return $(this);
+			},
+			setSingleTag: [
+				function () {
+					$(this).css({
+						backgroundColor: '#FF8C00'
+					});
+				} ],
 			setItemsArea: function () {
 				$(this).css({
 					width: 240,
 					height: document.body.clientHeight - 50,
-					// height: 300,
 					overflowY: 'auto',
-					// border: '1px solid black',
 					zIndex: 73,
 					marginLeft: 5,
 					marginTop: 50,
 					backgroundColor: '#5E5E5E'
 				});
+			},
+			setSelOpt: function () {
+				$(document.createElement('option')).text('对选中数据做并集运算').attr({
+					type: 'union'
+				}).appendTo($(this));
+				$(document.createElement('option')).text('对选中数据做交集运算').attr({
+					type: 'intersect'
+				}).appendTo($(this));
+				$(document.createElement('option')).text('对选中数据做差集运算').attr({
+					type: 'diff'
+				}).appendTo($(this));
+			},
+			setFilter: function () {
+				//TODO: 
 			}
 		},
 		_create: function () {
@@ -84,33 +101,14 @@
 			}, this.options.containerPos));
 			this._reSize(this.element);
 
-			// Handler
-			this._cartHandler = $('<div></div>')
-				.css($.extend({
-					position: 'absolute',
-					zIndex: this.options.maskZIndex + 3,
-					background: this.options.handlerBackground
-				}, this.options.handlerPos))
-				.addClass('cart-handler')
-				.appendTo(this.element);
-			this._reSize(this._cartHandler);
-
-			// Panel
-			this._innerPanel = $('<div></div>')
-				.css($.extend({
-					position: 'absolute',
-					zIndex: this.options.maskZIndex + 2,
-					background: this.options.panelBackground
-				}, this.options.panelPos))
-				.addClass('inner-panel')
-				.appendTo(this.element);
-			this._reSize(this._innerPanel);
+			this._setHandler();    // Handler
+			this._setPanel();    // Panel
 			
+			// var iarea = this._addItemsArea(this._innerPanel);    // Add items panel
+			// var btn = this._addButton(this._innerPanel);    // Add submit button
+			
+			// Add hook			
 			var hooks = this._addTag(this._cartHandler);    // Add tag
-			var iarea = this._addItemsArea(this._innerPanel);    // Add items panel 
-			var btn = this._addButton(this._innerPanel);    // Add submit button
-			
-			// Add trigger
 			this._addHook(hooks, 'click', this.element);
 		},
 		_reSize: function (element) {
@@ -119,7 +117,7 @@
 			$(window).resize(function () {
 				element.css({ height: document.body.clientHeight - (iWinHeight - that.options.height) });
 			});
-		},
+		},    // DONE
 		_setConPos: function (pos) {
 			switch (pos) {
 			case 'right':
@@ -186,37 +184,213 @@
 				};
 			}
 		},    // DONE
+		_setHandler: function () {
+			this._cartHandler = $('<div></div>')
+				.css($.extend({
+					position: 'absolute',
+					zIndex: this.options.maskZIndex + 3,
+					background: this.options.handlerBackground
+				}, this.options.handlerPos))
+				.addClass('cart-handler')
+				.appendTo(this.element);
+			this._reSize(this._cartHandler);
+			return this._cartHandler;
+		},    // DONE
+		_setPanel: function () {
+			this._innerPanel = $('<div></div>')
+				.css($.extend({
+					position: 'absolute',
+					zIndex: this.options.maskZIndex + 2,
+					background: this.options.panelBackground
+				}, this.options.panelPos))
+				.addClass('inner-panel')
+				.appendTo(this.element);
+			this._reSize(this._innerPanel);
+			this._setNotifyArea();
+			this._setItemsArea();
+			this._setBtnArea();
+			return this._innerPanel;
+		},    // DONE
+		_setNotifyArea: function () {
+			this._notifyArea = this._appendElement('div', 'notify-area', this._innerPanel);
+			this._notifyArea.css({
+				float: 'left',
+				width: '100%',
+				height: this.options.notifyHgt,
+				backgroundColor: this.options.panelBackground
+			});
+			return this._notifyArea;
+		},
+		_setItemsArea: function () {
+			this.itemArea = this._appendElement('div', 'item-area', this._innerPanel);
+			this.itemArea.css({
+				float: 'left',
+				width: '100%',
+				height: this.itemArea.parent().height() - this.options.notifyHgt - this.options.BtnHgt
+			});
+			this._setItemCon();
+			return this.itemArea;
+		},
+		_setItemCon: function () {
+			var margin_left = 5,
+				margin_top = 5;
+			this._itemCon = this._appendElement('div', 'item-con', this.itemArea);
+			this._itemCon.css({
+				float: 'left',
+				height: this._itemCon.parent().height() - margin_top,
+				width: this.options.panelWidth - (margin_left * 2),
+				marginTop: margin_top,
+				marginLeft: margin_left,
+				overflowY: 'auto',
+				zIndex: this.options.maskZIndex + 3,
+				backgroundColor: '#5E5E5E'
+			});
+			this.options.setItemsArea.call(this._itemCon);
+			return this._itemCon;
+		},
+		_setBtnArea: function () {
+			this.btnArea = this._appendElement('div', 'btn-area', this._innerPanel);
+			this.btnArea.css({
+				float: 'left',
+				width: '100%',
+				height: this.options.BtnHgt,
+				backgroundColor: '#DCDCDC'
+			});
+			this._setSubmitCtrl();
+			this._setFilter();
+			this._setSubmitBtn();
+			return this.btnArea;
+		},
+		_setSubmitCtrl: function () {
+			this.submitCtrl = this._appendElement('div', 'submit-ctrl', this.btnArea);
+			this.submitCtrl.css({
+				float: 'left',
+				width: '100%',
+				height: this.options.BtnHgt / 2
+			});
+			if (this.options.extraInfo)
+				this._setExtraInfo();
+			return this.submitCtrl;
+		},
+		
+		_setExtraInfo: function () {
+			var margin_left = 10,
+				bottom = 10;
+			this.extraInfo = this._appendElement('div', 'extra-info', this.submitCtrl);
+			this.extraInfo.css({
+				float: 'left',
+				width: this.extraInfo.parent().width() - (margin_left * 2),
+				marginLeft: margin_left,
+				height: this.options.BtnHgt / 2 - bottom
+			});
+			this._setSelect();
+			this._setUnfold();
+			return this.extraInfo;
+		},
+		_setSelect: function () {
+			this.selectArea = this._appendElement('div', 'select-area', this.extraInfo);
+			this.selector = this._appendElement('select', 'selector', this.selectArea);
+			this.selector.css({
+				float: 'left',
+				marginTop: 10,
+				boxShadow: 'inset 1px 2px 5px 1px #e2e2e2',
+				cursor: 'pointer',
+				borderRadius: 3,
+				height: 30,
+				backgroundColor: 'white',
+				width: 160
+			});
+			this.options.setSelOpt.call(this.selector);
+			return this.selector;
+		},
+		_setUnfold: function () {
+			var context = this;
+			this.unfold = this._appendElement('div', 'unfold', this.extraInfo);
+			this.unfold.css({
+				float: 'right',
+				marginTop: 10,
+				cursor: 'pointer',
+				borderRadius: 3,
+				height: 30,
+				border: '1px solid white',
+				width: 30
+			}).click(function () {
+				var $extra = $(this).parent(),
+					$filter = $extra.parent().next();
+					$submit = $extra.parent().next().next(),
+					$btnArea = $extra.parent().parent(),
+					$itemArea = $btnArea.prev();
+
+				if (context.options.unfold) {
+					$itemArea.animate({ height: '+=220px' });
+					$itemArea.children().animate({ height: '+=220px' });
+					$filter.animate({ height: '-=220px' });
+					$btnArea.animate({ height: '-=220px' });
+					context.options.unfold = false;
+				} else {
+					$itemArea.animate({ height: '-=220px' });
+					$itemArea.children().animate({ height: '-=220px' });
+					$filter.animate({ height: '+=220px' });
+					$btnArea.animate({ height: '+=220px' });
+					context.options.unfold = true;
+				}
+			});
+			return this.unfold;
+		},
+		_setFilter: function () {
+			this.filter = this._appendElement('div', 'filter-info', this.btnArea);
+			this.filter.css({
+				float: 'left',
+				width: '100%'
+			});
+			this.options.setFilter.call(this.filter);
+			return this.filter;
+		},
+		_setSubmitBtn: function () {
+			var margin_left = 10,
+				bottom = 10,
+				context = this;
+			this.submitBtn = this._appendElement('div', 'submit-btn', this.btnArea);
+			this.submitBtn.css({
+				float: 'left',
+				width: this.submitBtn.parent().width() - (margin_left * 2),
+				marginLeft: margin_left,
+				height: this.options.BtnHgt / 2 - bottom,
+				backgroundColor: '#8B0000',
+				cursor: 'pointer'
+			}).click(function () {
+				context._trigger('submit');
+			});
+			return this.submitBtn;
+		},
 		_addTag: function (handler) {    // Shall controll css from the outside
+			var context = this;
 			for(var i = 0; i < this.options.tags; i++) {
-				var hgtFrom = 90,
-					hgtTo = 120;
-				var tagIns = $('<div></div>')
+				var tagIns = $(document.createElement("div"))
 						.addClass('tag-ins off')
-						.appendTo(handler);
+						.appendTo(handler)
+						.click(function () {
+							context._trigger('refresh');
+						});
 				this.options.setCommonTag.call(tagIns);
+				this.options.commonTagEffect.call(tagIns);
 				if (this.options.setSingleTag[i] != null)
 					this.options.setSingleTag[i].call(tagIns);
 			}
 			return $('.tag-ins');
-		},
-		addTag: function (handler, tNum, tfoo) {
-			this._addTag(handler);    // Call from the outside
 		},
 		_addItemsArea: function (panel) {
 			this._itemsArea = $('<div></div>')
 				.addClass('items-area')
 				.appendTo(panel);
 			this.options.setItemsArea.call(this._itemsArea);
-			// this._itemIns = $(document.createElement("div")).addClass('item-ins').appendTo(this._itemsArea);
-			// this._itemIns.item({
-			// 	timeDft: "2014-12-13~2015-01-12"
-			// });
 			return $('.items-area');
 		},
 		_addButton: function (panel) {
-			var btnAreaWidth = 50;
+			var btnAreaWidth = 140;
 			this._panelBtn = $('<div></div>')
 				.css({
+					float: 'left',
 					height: btnAreaWidth,
 					width: this.options.panelWidth,
 					marginLeft: 0,
@@ -290,6 +464,9 @@
 		addHook: function (anchor, event, folddiv) {
 			this._addHook(anchor, event, folddiv);
 		},    // Call from the outside
+		_appendElement: function (type, cls, con) {
+			return $(document.createElement(type)).addClass(cls).appendTo(con);
+		},
 		destroy: function () {
 			this._cartHandler.remove();
 			this._innerPanel.remove();
